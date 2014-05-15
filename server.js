24e1ddd07824938b0675a037b3bc029379f59f98
Danny Grove <danny@drgrovellc.com>
2014-05-15 01:29:47 -0400

    Adds config options.

--- server.js
+++ server.js
@@ -2,17 +2,15 @@
 
 var express = require('express')
     require('express-namespace')
-    require('express-mongoose')
 var config = require('./lib/config'),
     response = require('./lib/response'),
     space = require('./lib/space'),
-  search = require('./lib/search')
+    search = require('./lib/search')
 
 var app = express();
 
-app.namespace('/api', function(){
+app.namespace(config.app.namespace, function(){
     app.post('space/add', space.addSpace)
-
     app.get('search/:space', search.getSpaces)
 });
 
